42caada96d00d42f7b2e6780b5d72e30830ea083
Shaun Hamilton <shauhami020@gmail.com>
2022-01-18 14:08:52 +0000

    chore: add initial configs and openCourse cmd

---
+++ src/course-input.ts
@@ -0,0 +1,49 @@
+import { ExtensionContext, window } from "vscode";
+import fetch from "node-fetch";
+
+interface Course {
+  name: string;
+  githubLink: string;
+  tags: string[];
+}
+
+interface Courses {
+  courses: Course[];
+}
+
+/**
+ * Shows a pick list using window.showQuickPick().
+ */
+export async function courseInput(context: ExtensionContext) {
+  let i = 0;
+  const { courses } = (await (
+    await fetch(
+      "https://raw.githubusercontent.com/ShaunSHamilton/courses-plus/main/resources/courses.json"
+    )
+  ).json()) as Courses;
+  const result = await window.showQuickPick(
+    courses.map(({ name }) => name),
+    {
+      placeHolder: "Select a course",
+      onDidSelectItem: (course) =>
+        window.showInformationMessage(`Opening Course: ${course}`),
+    }
+  );
+  window.showInformationMessage(`Got: ${result}`);
+}
+
+/**
+ * Shows an input box using window.showInputBox().
+ */
+export async function showInputBox() {
+  const result = await window.showInputBox({
+    value: "abcdef",
+    valueSelection: [2, 4],
+    placeHolder: "For example: fedcba. But not: 123",
+    validateInput: (text) => {
+      window.showInformationMessage(`Validating: ${text}`);
+      return text === "123" ? "Not 123!" : null;
+    },
+  });
+  window.showInformationMessage(`Got: ${result}`);
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,53 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, window, ExtensionContext } from "vscode";
+import { courseInput } from "./course-input";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "freecodecamp-courses-plus" is now active!');
+export function activate(context: ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Your extension "freecodecamp-courses" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('freecodecamp-courses-plus.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from freeCodeCamp - Courses Plus!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    commands.registerCommand("freecodecamp-courses.openCourse", async () => {
+      courseInput(context);
+    })
+  );
+  // The code you place here will be executed every time your command is executed
+  // Display a message box to the user
+  // openSimpleBrowser();
+  // startLiveServer();
+  // startWatcher();
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+/**
+ * This function opens the built-in VSCode Simple Browser, and loads the local port started by live-server
+ */
+export function openSimpleBrowser() {
+  commands.executeCommand("simpleBrowser.show", "http://127.0.0.1:8080/");
+}
+
+export function openTerminal() {}
+
+/**
+ * This function runs `live-server --port=8080 --entry-file=temp.html` in the background
+ */
+export function startLiveServer() {
+  const terminal = window.createTerminal("freeCodeCamp: Live Server");
+  terminal.sendText("live-server --port=8080 --entry-file=temp.html");
+}
+
+export function stopLiveServer() {}
+
+/**
+ * This function runs `node ./tooling/hot-reload.js` in the background
+ */
+export function startWatcher() {
+  const terminal = window.createTerminal("freeCodeCamp: Watcher");
+  terminal.sendText("node ./tooling/hot-reload.js");
+}
