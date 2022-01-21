435c759eeb8df4d21a51836080a500fc27ca5332
Shaun Hamilton <shauhami020@gmail.com>
2022-01-21 21:45:09 +0000

    chore(0.3.2): fix handle for multiple processes

--- src/handles.ts
+++ src/handles.ts
@@ -20,7 +20,9 @@ export function handleMessage(flash: Flash) {
  * handleTerminal("freeCodeCamp: Open Course", "git clone something", "npm install", "live-server .")
  */
 export function handleTerminal(name: string, ...commands: string[]) {
-  const commandString = commands.join(" && ").replace(/&& & &&/g, "&");
+  const commandString = commands
+    .join(" && ")
+    .replace(/ ([^&]+) && & && ([^&]+)/g, " ?($1 & $2)");
   const terminal = window.createTerminal(name);
   terminal.sendText(commandString, true);
   return terminal;
