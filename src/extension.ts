ccef47f770688046f8e7b368a05a7a3a22a98781
Shaun Hamilton <shauhami020@gmail.com>
2022-01-22 15:43:45 +0000

    chore: adjust test

--- src/extension.ts
+++ src/extension.ts
@@ -52,8 +52,9 @@ export function activate(context: ExtensionContext) {
   );
   context.subscriptions.push(
     commands.registerCommand("freecodecamp-courses.test", async () => {
-      const isConnected = await isConnectedToInternet();
-      console.log(isConnected);
+      const path = Uri.file(workspace.workspaceFolders?.[0]?.uri?.fsPath ?? "");
+      const arrOfArrs = await workspace.fs.readDirectory(path);
+      console.log(arrOfArrs, path);
     })
   );
 }
