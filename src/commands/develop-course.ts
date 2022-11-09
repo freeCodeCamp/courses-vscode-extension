81c04cbac1c13db66e15986b629d68e075771667
Shaun Hamilton <shauhami020@gmail.com>
2022-11-09 13:31:10 +0000

    fix(1.7.1): prevent fail-fast on version check (#170)
    * patch(1.7.1): prevent fail-fast on version check
    
    * feat: improve error messages

--- src/commands/develop-course.ts
+++ src/commands/develop-course.ts
@@ -1,16 +1,27 @@
 import { handleConfig } from "../handles";
 import { handleMessage } from "../flash";
 import { FlashTypes } from "../typings";
-import { getConfig } from "../usefuls";
+import { getConfig, getPackageJson } from "../usefuls";
+import { checkForCourseUpdates } from "../updates";
 
 export default async function developCourse() {
   try {
     const config = await getConfig();
+    const rootPackage = await getPackageJson();
+    const githubLink = rootPackage.repository.url;
+    const isCourseUpdates = await checkForCourseUpdates(githubLink, config);
+    if (isCourseUpdates) {
+      handleMessage({
+        message:
+          "This course has been updated. It is recommended you re-clone the repository.",
+        type: FlashTypes.WARNING,
+      });
+    }
     handleConfig(config, "develop-course");
   } catch (e) {
     console.error("freeCodeCamp > runCourse: ", e);
     return handleMessage({
-      message: "Unable to find a `freecodecamp.conf.json` file in workspace.",
+      message: "Unable to develop course. See dev console for more details.",
       type: FlashTypes.ERROR,
     });
   }
