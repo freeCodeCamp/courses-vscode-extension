f18444dc211093c00264c574b5976d4ef0ba676d
Shaun Hamilton <shauhami020@gmail.com>
2023-06-26 20:24:05 +0200

    clone example folder from freecodecamp-os

--- src/commands/create-new-course.ts
+++ src/commands/create-new-course.ts
@@ -6,21 +6,17 @@ export default async function createNewCourse() {
   // Ensure directory is empty
   await handleEmptyDirectory();
   const terminalId = "freeCodeCamp: Create New Course";
+
+  // Clone just the contents of the `minimal-example` directory from the `main` branch of `https://github.com/freeCodeCamp/freeCodeCampOS`
   const commands = [
-    "npm init -y",
-    "npm i @freecodecamp/freecodecamp-os",
-    "touch freecodecamp.conf.json",
-    'echo \'{"path": ".","version":"0.0.1","scripts":{"develop-course": "NODE_ENV=development node ./node_modules/@freecodecamp/freecodecamp-os/.freeCodeCamp/tooling/server.js","run-course": "NODE_ENV=production node ./node_modules/@freecodecamp/freecodecamp-os/.freeCodeCamp/tooling/server.js"},"curriculum":{"locales":{"english":"./curriculum/locales/english"}}}\' > freecodecamp.conf.json',
-    "mkdir -p curriculum/locales/english",
-    "touch curriculum/locales/english/example.md",
-    'echo "# Example - Course\n\nThis is an example course.\n\n## 1\n\n## --fcc-end--\n" > curriculum/locales/english/example.md',
-    "mkdir config",
-    "touch config/projects.json",
-    'echo \'[{"id": 0, "title": "Example", "dashedName": "example"}]\' > config/projects.json',
-    "touch config/state.json",
-    'echo \'{"currentProject": null, "locale": "english"}\' > config/state.json',
-    "mkdir example",
-    "touch example/.gitkeep",
+    `git clone -n --depth=1 --filter=tree:0 https://github.com/freeCodeCamp/freeCodeCampOS.git`,
+    `cd freeCodeCampOS`,
+    `git sparse-checkout set --no-cone minimal-example`,
+    `git checkout`,
+    `mv minimal-example/* ../`,
+    `cd ..`,
+    `rm -rf freeCodeCampOS`,
+    `exit`,
   ];
 
   const terminal = handleTerminal(".", terminalId, ...commands);
