630373ec60e2b99b87251bb3c831135bceb3dddd
Shaun Hamilton <shauhami020@gmail.com>
2024-01-25 14:46:57 +0200

    fix: remove mention of node-fetch

--- src/components.ts
+++ src/components.ts
@@ -1,5 +1,4 @@
 import { commands, Uri, workspace, window } from "vscode";
-import fetch from "node-fetch";
 import { Course, FlashTypes } from "./typings";
 import { handleMessage } from "./flash";
 
