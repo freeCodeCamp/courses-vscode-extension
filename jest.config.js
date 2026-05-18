91dce25b232cc2532aac782d8e2baa447f111925
test <test@test.com>
2026-05-18 15:00:11 +1000

    test: add test configuration

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  moduleNameMapper: {
+    "^vscode$": "<rootDir>/src/__mocks__/vscode.js", // Map the 'vscode' module to a mock
+  },
+};
